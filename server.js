f12f3a1a5e9fa2029ed488cac538c1f0a1367c22
Aashutosh <ashutoshnishad2003@gmail.com>
2022-10-05 19:01:25 -0700

    first commit

--- server.js
+++ server.js
@@ -22,6 +22,6 @@ app.use("/user" , require("./Routes/auth"));
 //     res.send("I am working")
 // })
 
-app.listen(port, ()=>{
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  })
