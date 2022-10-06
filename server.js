57c2076e609aa693a64da1d6a1a70a34a7a5f3b7
Aashutosh <ashutoshnishad2003@gmail.com>
2022-10-06 02:24:06 -0700

    unknow error

--- server.js
+++ server.js
@@ -8,16 +8,13 @@ const port = process.env.PORT | 5000;
 // app.get('/', (req,res)=>{
 //     res.send("Hello World!")
 // })
-app.use(express.static("./build"))
+// app.use(express.static("./build"))
 app.use(cors());
 //express.json will allow us to send request body content as middleware
 app.use(express.json());
 //Adding routes from the routes folder:
 app.use("/" , require("./Routes/request"));
 app.use("/user" , require("./Routes/auth"));
-app.get("/" ,(req,res)=>{
-    res.send("I am working")
-})
 
 app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
