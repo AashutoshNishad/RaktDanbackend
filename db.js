1655e9e9e8159428162ff5ea14e621526276622e
Aashutosh <ashutoshnishad2003@gmail.com>
2022-10-05 19:57:08 -0700

    first commit

--- db.js
+++ db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-// const db_url = process.env.DB_URL || "mongodb+srv://nssgecr:9753318620@cluster0.u1gdxdi.mongodb.net/?retryWrites=true&w=majority";
+const db_url = process.env.DB_URL;
 
 const connectToMongo = ()=>{
     try {
